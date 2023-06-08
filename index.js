9dbd57fa933ca04ee84280732a4ed63218337092
EMAYAN VADIVEL <em241005@dal.ca>
2023-06-08 15:22:43 -0300

    Fixing logic error in the index.js file for container2

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const csv = require('csv-parser');
-const fs = require('fs');
+const csv = require('csv-parser'); // REFERENCE: https://www.npmjs.com/package/csv-parser
+const fs = require('fs'); // REFERENCE: https://www.npmjs.com/package/csv-parser
 const path = require('path');
-// const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
@@ -15,65 +14,63 @@ app.post('/calculate', (req, res) => {
     return res.status(400).json({ file: null, error: 'Invalid JSON input.' });
   }
 
-  // Create the absolute path of the file
-  const filePath = path.join('/Emayan_PV_dir', file); // Replace "xxxx" with your first name
+  // Get the absolute path of the file
+  const filePath = path.join("/Emayan_PV_dir",file);
 
   // Check if the file exists
-  fs.access(filePath, fs.constants.F_OK, (err) => {
-    if (err) {
-      return res.status(404).json({ file, error: 'File not found.' });
-    }
+  if (!fs.existsSync(filePath)) {
+    return res.status(404).json({ file, error: 'File not found.' });
+  }
 
-    let headerFound = false; // Variable to track if the header is found
-    let fileError = false; // Variable to track if any error is found
+  let headerFound = false; // Variable to track if the header is found
+  let fileError = false; // Variable to track if any error is found
 
-    fs.createReadStream(filePath)
-      .pipe(csv())
-      .on('data', (row) => {
-        if (!headerFound) {
-          // Check if the header is present
-          if (!row.product || !row.amount) {
-            headerFound = true; // Set headerFound to true to prevent sending multiple responses
-            fileError = true;
-          }
-          headerFound = true;
-        } else {
-          // Check if any row is missing a comma
-          if (!row.product || !row.amount || row.product.includes(',') || row.amount.includes(',')) {
-            fileError = true;
-          }
+  fs.createReadStream(filePath)
+    .pipe(csv())
+    .on('data', (row) => {
+      if (!headerFound) {
+        // Check if the header is present
+        if (!row.product || !row.amount) {
+          headerFound = true; // Set headerFound to true to prevent sending multiple responses
+          fileError = true;
         }
-      })
-      .on('end', () => {
-        if (!headerFound || fileError) {
+        headerFound = true;
+      } else {
+        // Check if any row is missing a comma
+        if (!row.product || !row.amount || row.product.includes(',') || row.amount.includes(',')) {
+          fileError = true;
+        }
+      }
+    })
+    .on('end', () => {
+      if (!headerFound || fileError) {
+        return res
+          .status(200)
+          .json({ file, error: 'Input file not in CSV format.' });
+      }
+
+      const rows = [];
+      fs.createReadStream(filePath)
+        .pipe(csv())
+        .on('data', (row) => {
+          rows.push(row);
+        })
+        .on('end', () => {
+          const sum = rows
+            .filter((row) => row.product === product)
+            .reduce((total, row) => total + parseInt(row.amount), 0);
+
+          return res.json({ file, sum });
+        })
+        .on('error', () => {
           return res
             .status(200)
             .json({ file, error: 'Input file not in CSV format.' });
-        }
-
-        const rows = [];
-        fs.createReadStream(filePath)
-          .pipe(csv())
-          .on('data', (row) => {
-            rows.push(row);
-          })
-          .on('end', () => {
-            const sum = rows
-              .filter((row) => row.product === product)
-              .reduce((total, row) => total + parseInt(row.amount), 0);
-
-            return res.json({ file, sum });
-          })
-          .on('error', () => {
-            return res
-              .status(200)
-              .json({ file, error: 'Input file not in CSV format.' });
-          });
-      });
-  });
+        });
+    });
 });
 
 const PORT = 7000;
 app.listen(PORT, () => {
-  console.log(`Container 2 is running on port ${PORT}`);
+  console.log(`Container 2 listening on port ${PORT}`); //REFERENCE: https://www.w3schools.com/nodejs/met_server_listen.asp
 });
