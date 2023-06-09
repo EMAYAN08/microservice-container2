baaf80bde77a96fb7da5fa57b11f28159ea6dcb3
EMAYAN VADIVEL <em241005@dal.ca>
2023-06-09 16:02:19 -0300

    changing index.js file

--- index.js
+++ index.js
@@ -7,58 +7,68 @@ const app = express();
 app.use(express.json());
 
 // Endpoint to handle calculate requests
-// Update the /calculate endpoint in container2/index.js
 app.post('/calculate', (req, res) => {
   const { file, product } = req.body;
 
-  if (!file || !product) {
+  if (!file) {
     return res.status(400).json({ file: null, error: 'Invalid JSON input.' });
   }
 
-  const filePath = path.join('/Emayan_PV_dir', file);
+  // Get the absolute path of the file
+  const filePath = path.join("/Emayan_PV_dir",file);
 
+  // Check if the file exists
   if (!fs.existsSync(filePath)) {
     return res.status(404).json({ file, error: 'File not found.' });
   }
 
-  let headerFound = false;
-  let fileError = false;
-  const rows = [];
+  let headerFound = false; // Variable to track if the header is found
+  let fileError = false; // Variable to track if any error is found
 
   fs.createReadStream(filePath)
     .pipe(csv())
-    .on('headers', (headers) => {
-      if (!headers.includes('product') || !headers.includes('amount')) {
-        fileError = true;
-      }
-      headerFound = true;
-    })
     .on('data', (row) => {
-      if (headerFound) {
+      if (!headerFound) {
+        // Check if the header is present
+        if (!row.product || !row.amount) {
+          headerFound = true; // Set headerFound to true to prevent sending multiple responses
+          fileError = true;
+        }
+        headerFound = true;
+      } else {
+        // Check if any row is missing a comma
         if (!row.product || !row.amount || row.product.includes(',') || row.amount.includes(',')) {
           fileError = true;
-        } else {
-          rows.push(row);
         }
       }
     })
     .on('end', () => {
       if (!headerFound || fileError) {
-        return res.status(200).json({ file, error: 'Input file not in CSV format.' });
+        return res
+          .status(200)
+          .json({ file, error: 'Input file not in CSV format.' });
       }
 
-      const sum = rows
-        .filter((row) => row.product === product)
-        .reduce((total, row) => total + parseInt(row.amount), 0);
+      const rows = [];
+      fs.createReadStream(filePath)
+        .pipe(csv())
+        .on('data', (row) => {
+          rows.push(row);
+        })
+        .on('end', () => {
+          const sum = rows
+            .filter((row) => row.product === product)
+            .reduce((total, row) => total + parseInt(row.amount), 0);
 
-      return res.json({ file, sum });
-    })
-    .on('error', () => {
-      return res.status(200).json({ file, error: 'Input file not in CSV format.' });
+          return res.json({ file, sum });
+        })
+        .on('error', () => {
+          return res
+            .status(200)
+            .json({ file, error: 'Input file not in CSV format.' });
+        });
     });
 });
-
-
 
 const PORT = 7000;
 app.listen(PORT, () => {
