639da6e6874c631e118ce7eb7c35ccc6cdd8ca3b
EMAYAN VADIVEL <em241005@dal.ca>
2023-06-11 14:23:02 -0300

    making final changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const csv = require('csv-parser'); // REFERENCE: https://www.npmjs.com/package/csv-parser
-const fs = require('fs'); // REFERENCE: https://www.npmjs.com/package/csv-parser
+const fs = require('fs');
 const path = require('path');
+const csv = require('fast-csv');
 
 const app = express();
 app.use(express.json());
@@ -15,7 +15,7 @@ app.post('/calculate', (req, res) => {
   }
 
   // Get the absolute path of the file
-  const filePath = path.join("/Emayan_PV_dir",file);
+  const filePath = path.join('/Emayan_PV_dir', file);
 
   // Check if the file exists
   if (!fs.existsSync(filePath)) {
@@ -25,8 +25,13 @@ app.post('/calculate', (req, res) => {
   let headerFound = false; // Variable to track if the header is found
   let fileError = false; // Variable to track if any error is found
 
+  const rows = [];
+
   fs.createReadStream(filePath)
-    .pipe(csv())
+    .pipe(csv.parse({ headers: true, trim: true })) // Parse CSV with headers and trim whitespace
+    .on('error', () => {
+      return res.status(200).json({ file, error: 'Input file not in CSV format.' });
+    })
     .on('data', (row) => {
       if (!headerFound) {
         // Check if the header is present
@@ -36,41 +41,28 @@ app.post('/calculate', (req, res) => {
         }
         headerFound = true;
       } else {
-        // Check if any row is missing a comma
-        if (!row.product || !row.amount || row.product.includes(',') || row.amount.includes(',')) {
+        // Check if any row is missing a value
+        if (!row.product || !row.amount) {
           fileError = true;
         }
       }
+
+      rows.push(row);
     })
     .on('end', () => {
       if (!headerFound || fileError) {
-        return res
-          .status(200)
-          .json({ file, error: 'Input file not in CSV format.' });
+        return res.status(200).json({ file, error: 'Input file not in CSV format.' });
       }
 
-      const rows = [];
-      fs.createReadStream(filePath)
-        .pipe(csv())
-        .on('data', (row) => {
-          rows.push(row);
-        })
-        .on('end', () => {
-          const sum = rows
-            .filter((row) => row.product === product)
-            .reduce((total, row) => total + parseInt(row.amount), 0);
+      const sum = rows
+        .filter((row) => row.product === product)
+        .reduce((total, row) => total + parseInt(row.amount), 0);
 
-          return res.json({ file, sum });
-        })
-        .on('error', () => {
-          return res
-            .status(200)
-            .json({ file, error: 'Input file not in CSV format.' });
-        });
+      return res.json({ file, sum });
     });
 });
 
 const PORT = 7000;
 app.listen(PORT, () => {
-  console.log(`Container 2 listening on port ${PORT}`); //REFERENCE: https://www.w3schools.com/nodejs/met_server_listen.asp
+  console.log(`Container 2 listening on port ${PORT}`);
 });
